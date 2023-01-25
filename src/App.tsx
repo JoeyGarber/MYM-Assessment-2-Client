2667bee23712e10292533a20b6064d5d26b25b7a
joeygarber <joseph.z.garber@gmail.com>
2023-01-24 21:36:11 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import SignIn from './components/sign-in';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [ APODUrl, setAPODUrl ] = useState(null)
+  const [ user, setUser ] = useState(null)
+
+  useEffect(() => {
+    // React handles .env files as long as the keys start with REACT_APP
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_APIKEY}`)
+    .then((response) => response.json())
+    .then(data => setAPODUrl(data.url))
+  }, [])
+
+  if (user) {
+    return (
+      <>
+        {APODUrl && <img src={APODUrl} />}
+      </>
+    );
+  } else {
+    return (
+        <SignIn />
+    )
+  }
+
+  
 }
 
 export default App;
---
+++ src/components/sign-in.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+function SignIn () {
+  return (
+    <>
+      <p>Test</p>
+    </>
+  )
+}
+
+export default SignIn
