05d8e5096d7f2782ecad6c2eb5d0343cf0b99fae
joeygarber <joseph.z.garber@gmail.com>
2023-01-25 23:53:52 -0500

    Added in logout, set logged in users to see image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
 
   const [ APODUrl, setAPODUrl ] = useState(null)
   const [ user, setUser ] = useState(null)
-
 
   useEffect(() => {
     // React handles .env files as long as the keys start with REACT_APP
@@ -14,18 +14,35 @@ function App() {
     .then(data => setAPODUrl(data.url))
   }, [])
 
+  useEffect(() => {
+    if (!user) {
+      axios.get('http://localhost:8080/user', { withCredentials: true })
+      .then(response => {
+        console.log(response)
+        setUser(response.data)
+      })
+    }
+  }, [])
+
+  const onLogOut = () => {
+    setUser(null)
+    axios.post('http://localhost:8080/logout', {withCredentials: true})
+  }
+
   if (user) {
     return (
-      <>
+      <div className='image'>
         {APODUrl && <img src={APODUrl} />}
-      </>
+        <h3>Pretty cool, huh? Nasa, am I right?</h3>
+        <button onClick={onLogOut}>Log Out</button>
+      </div>
     );
   } else {
     return (
-      <>
-        <p>Please sign in</p>
-        <button onClick={() => window.location.replace('http://localhost:8080/google')}>Click me</button>
-      </>
+      <div className='sign-in'>
+        <h3>Sign In with Google to See Today's Cool Space Image</h3>
+        <button onClick={() => window.location.assign('http://localhost:8080/google')}>Sign In</button>
+      </div>
     )
   }
 }
